63a89471e48397c6843c05ca01b079b0de70e3dc
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2022-02-06 09:25:01 +0100

    feat: add types for next's configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+// @ts-check
 const compose = require('next-compose-plugins');
 
 const plugins = [];
 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfiguration = {
-  /* Next.js configuration */
+  /* config options here */
 };
 
 module.exports = compose(plugins, nextConfiguration);
