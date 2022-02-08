13ed7c7e98d9ee1db8f6b80cab33928edc554575
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2022-02-08 17:18:32 +0100

    feat: ctl error

--- cypress/e2e/homepage.spec.ts
+++ cypress/e2e/homepage.spec.ts
@@ -1,5 +1,5 @@
 describe('Homepage', () => {
-  it('should contain "Hello World!" title', async () => {
+  it('should contain "Hello World!" title', () => {
     cy.visit('/')
       .findByText(/hello world!/i)
       .should('exist');
