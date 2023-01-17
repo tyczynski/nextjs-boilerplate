c9166da9e02353bbb916901e9524a420f9699cc3
Przemysław Tyczyński <przemyslaw@tyczynski.pl>
2023-01-17 19:09:59 +0100

    Configure git hooks with `husky` and `lint-staged`

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,10 +1,26 @@
-const path = require('path');
+const { quote } = require('shell-quote');
+const { ESLint } = require('eslint');
 
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(' --file ')}`;
+const eslint = new ESLint();
+const isWin = process.platform === 'win32';
+
+function escape(str) {
+  const escaped = quote(str);
+  return escaped.replace(/\\@/g, '@');
+}
 
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': (filenames) => {
+    const escapedFileNames = filenames
+      .map((filename) => (isWin ? filename : escape([filename])))
+      .join(' ');
+
+    return [
+      `prettier --with-node-modules --ignore-path .prettierignore_staged --write ${escapedFileNames}`,
+      `eslint --no-ignore --max-warnings=0 --fix ${filenames
+        .filter((file) => !eslint.isPathIgnored(file))
+        .map((f) => `"${f}"`)
+        .join(' ')}`,
+    ];
+  },
 };
